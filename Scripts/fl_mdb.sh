609774b681988e07016b45dc03bc305f7600e282
JdoubleW <31694120+JdoubleW@users.noreply.github.com>
2020-02-26 15:44:41 +0100

    Update fl_mdb.sh

--- Scripts/fl_mdb.sh
+++ Scripts/fl_mdb.sh
@@ -1,9 +1,9 @@
 #Get updates
-yum update -y
+sudo yum update -y
 
 #Create repo file for mongodb
-touch /etc/yum.repos.d/mongodb-org-4.2.repo
-cat > /etc/yum.repos.d/mongodb-org-4.2.repo << EOF2
+sudo touch /etc/yum.repos.d/mongodb-org-4.2.repo
+sudo cat > /etc/yum.repos.d/mongodb-org-4.2.repo << EOF2
 [mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.2/x86_64/
@@ -13,15 +13,14 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc
 EOF2
 
 #Update repo, install mongodb and start the service.
-yum update -y
 sudo yum install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
 #Install python3 pip and different functions
-yum install -y python3-pip
-pip3 install flask Flask-PyMongo jsonify
+sudo yum install -y python3-pip
+sudo pip3 install flask Flask-PyMongo jsonify
 
 #Add firewall rules
-firewall-cmd --add-port=5000/tcp --permanent
-firewall-cmd --reload
+sudo firewall-cmd --add-port=5000/tcp --permanent
+sudo firewall-cmd --reload
